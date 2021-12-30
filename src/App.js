6298197806423e72885a96067b1d957f2613fd39
xthiagosant <uzmaki_thiago@outllok.com>
2021-12-30 14:33:32 -0300

    projeto limpo

--- src/App.js
+++ src/App.js
@@ -3,21 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>TSS Apostas Mega sena</h1>
       </header>
+      <main>
+        <h2>Apostas</h2>
+        <span>123456</span>
+      </main>
     </div>
   );
 }
