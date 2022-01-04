33f9b6460a691c4490a5458a317d9238151449a3
xthiagosant <uzmaki_thiago@outlook.com>
2022-01-04 17:42:13 -0300

    titulo

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -6,7 +6,7 @@ function NavBar() {
       <div className="d-flex flex-column text-center justify-content-center align-items-center flex-md-row p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm gap-3">
         <img src={cash} alt="TssWebApps" width="90" />
 
-        <p className="fs-2 m-0">Gerador de apostas</p>
+        <p className="fs-2 m-0">Gerador de apostas - Mega Sena(6 dezenas)</p>
       </div>
     </div>
   )
